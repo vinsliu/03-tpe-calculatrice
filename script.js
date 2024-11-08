797fee7ca119b78a1946f837351cd3cb0c8e42bd
Vincent Lav <vincent00lav@gmail.com>
2024-11-08 14:51:40 +0100

    Style done

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
+let showBtn = document.getElementById("show");
+let hideBtn = document.getElementById("hide");
 let screen = document.getElementById("screen");
 let keys = document.getElementById("keys");
