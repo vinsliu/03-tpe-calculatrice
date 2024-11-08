31b918bbe975c0dc7ec2a00e014912d6faf07b76
Vincent Lav <vincent00lav@gmail.com>
2024-11-08 15:08:52 +0100

    js form done

--- script.js
+++ script.js
@@ -1,4 +1,38 @@
 let showBtn = document.getElementById("show");
 let hideBtn = document.getElementById("hide");
+
+// Format formulaire
+document
+  .getElementById("calculatorForm")
+  .addEventListener("submit", function (e) {
+    e.preventDefault();
+    let firstInput = parseFloat(document.getElementById("firstInput").value);
+    let secondInput = parseFloat(document.getElementById("secondInput").value);
+    let operator = document.getElementById("operator").value;
+    console.log(operator);
+    let result;
+
+    if (isNaN(firstInput) || isNaN(secondInput)) {
+      result = "Veuillez entrer des veleurs valides.";
+    } else {
+      if (operator == "plus") {
+        result = firstInput + secondInput;
+      } else if (operator == "minus") {
+        result = firstInput - secondInput;
+      } else if (operator == "times") {
+        result = firstInput * secondInput;
+      } else if (operator == "divide") {
+        result = firstInput / secondInput;
+      } else {
+        result = "ERREUR !";
+      }
+    }
+
+    document.getElementById(
+      "form-display"
+    ).innerHTML = `<span>${result}</span>`;
+  });
+
+// Format calculatrice
 let screen = document.getElementById("screen");
 let keys = document.getElementById("keys");
