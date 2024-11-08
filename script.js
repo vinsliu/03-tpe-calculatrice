3438b9291be7884a96ec5aafabd13ba2bb049366
Vincent Lav <vincent00lav@gmail.com>
2024-11-08 15:35:24 +0100

    Form code done

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-let showBtn = document.getElementById("show");
-let hideBtn = document.getElementById("hide");
+document.getElementById("show").addEventListener("click", function () {
+  document.getElementById("calculator").style.display = "block";
+//   document.getElementById("calculatorForm").style.display = "block";
+});
+document.getElementById("hide").addEventListener("click", function () {
+  document.getElementById("calculator").style.display = "none";
+//   document.getElementById("calculatorForm").style.display = "none";
+});
 
 // Format formulaire
 document
@@ -15,13 +21,13 @@ document
     if (isNaN(firstInput) || isNaN(secondInput)) {
       result = "Veuillez entrer des veleurs valides.";
     } else {
-      if (operator == "plus") {
+      if (operator == "+") {
         result = firstInput + secondInput;
-      } else if (operator == "minus") {
+      } else if (operator == "-") {
         result = firstInput - secondInput;
-      } else if (operator == "times") {
+      } else if (operator == "*") {
         result = firstInput * secondInput;
-      } else if (operator == "divide") {
+      } else if (operator == "/") {
         result = firstInput / secondInput;
       } else {
         result = "ERREUR !";
@@ -31,8 +37,14 @@ document
     document.getElementById(
       "form-display"
     ).innerHTML = `<span>${result}</span>`;
+
+    if (!isNaN(result)) {
+      let formHist = document.getElementById("form-history");
+      let history = `<span>${firstInput} ${operator} ${secondInput} = ${result}</span>`;
+      formHist.insertAdjacentHTML("afterbegin", history);
+    }
   });
 
 // Format calculatrice
 let screen = document.getElementById("screen");
-let keys = document.getElementById("keys");
+document.getElementById("keys").addEventListener("click", function () {});
